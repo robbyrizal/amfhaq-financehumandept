ca55530354c3373ec2e4bb50791d7f85f5b6601d
Robi Rizal <robby.assist@gmail.com>
2020-12-21 19:34:45 +0700

    edit express static

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connection.once('open', () => {
 	console.log('connected to database');
 })
 
-app.use(express.static('client/build'));
+app.use(express.static(__dirname + 'client/build'));
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
