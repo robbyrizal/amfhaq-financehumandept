08935d75c2183b89dbda348e3bf3036e5f908b58
Robi Rizal <robby.assist@gmail.com>
2021-04-08 14:01:07 +0700

    add build @jurnalumum

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ec1fc53dbc6f2a96c3ea7648495d746.js"
+  "/precache-manifest.00d1c0e81d203a93e744e469440f3f01.js"
 );
 
 self.addEventListener('message', (event) => {
